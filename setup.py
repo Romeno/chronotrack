cafb2c26ee183fcdfb64a6caec12c02e692ec3c7
Romeno <RomenoEx@gmail.com>
2018-07-26 22:18:30 +0300

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="chronotrack",
-    version="0.0.1",
+    version="0.0.2",
     author="Romeno",
     author_email="RomenoEx@gmail.com",
     description="chronotrack API python interface",
